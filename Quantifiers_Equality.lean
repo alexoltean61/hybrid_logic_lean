e5842dc2f261807332d5cd713f06a5c180a4ac2a
Alex Oltean <alexoltean@mailbox.org>
2023-02-23 23:48:59 +0200

    done proofs

--- Quantifiers_Equality.lean
+++ Quantifiers_Equality.lean
@@ -96,7 +96,7 @@ example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) :=
       )
     )
 
-example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) :=
+theorem t1 : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) :=
     Iff.intro
     (fun h1 : ¬ ∀ x, p x =>
       byContradiction
@@ -178,3 +178,78 @@ example (a : α) : (∃ x, r → p x) ↔ (r → ∃ x, p x) :=
           )
         )
       )
+
+example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) :=
+    Iff.intro
+    (fun h1: ∀ x, p x ∧ q x =>
+      have ls : ∀ x, p x := (fun x => (h1 x).left)
+      have rs : ∀ x, q x := (fun x => (h1 x).right)
+      ⟨ls, rs⟩  
+    )
+    (fun h2 : (∀ x, p x) ∧ (∀ x, q x) =>
+      (fun x => ⟨h2.left x, h2.right x⟩)
+    )
+
+example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) :=
+    (fun h1 : ∀ x, p x → q x =>
+      (fun h2 : ∀ x, p x =>
+        fun x => (h1 x) (h2 x)
+      ) 
+    )
+
+example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x :=
+    (fun h : (∀ x, p x) ∨ (∀ x, q x) =>
+      Or.elim h
+      (fun hpx : ∀ x, p x => fun x => Or.inl (hpx x))
+      (fun hqx : ∀ x, q x => fun x => Or.inr (hqx x))
+    )
+
+example : α → ((∀ x : α, r) ↔ r) :=
+    (fun a : α =>
+      Iff.intro
+      (fun h1 : ∀ x : α, r => h1 a)
+      (fun hr : r => fun x => hr)
+    )
+
+example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r :=
+    Iff.intro
+    (fun h1 : ∀ x, p x ∨ r =>
+      byCases
+      (fun hpx : ∀ x, p x => Or.inl hpx)
+      (fun hnpx : ¬ ∀ x, p x =>
+        -- cutting some corners here:
+        have henpx : ∃ x, ¬ (p x) := (t1 α p).mp hnpx
+        match henpx with
+        | ⟨w, hw⟩ => (
+            Or.elim (h1 w)
+            (fun hpw : p w => absurd hpw hw)
+            (fun hr : r => Or.inr hr)
+        )   
+      )
+    )
+    (fun h2 : (∀ x, p x) ∨ r =>
+      Or.elim h2
+      (fun hpx : ∀ x, p x => fun x => Or.inl (hpx x))
+      (fun hr : r => fun x => Or.inr hr)
+    )
+example : (∀ x, r → p x) ↔ (r → ∀ x, p x) :=
+    Iff.intro
+    (fun h1 : ∀ x, r → p x =>
+      fun r => fun x => (h1 x) r
+    )
+    (fun h2 : r → ∀ x, p x =>
+      fun x => fun r => (h2 r) x
+    )
+
+
+variable (men : Type) (barber : men)
+variable (shaves : men → men → Prop)
+
+example (h : ∀ x : men, shaves barber x ↔ ¬ shaves x x) : False :=
+    Or.elim (em (shaves barber barber))
+    (fun h1 : (shaves barber barber) =>
+      show False from ((h barber).mp h1) h1
+    )
+    (fun h2 : ¬ (shaves barber barber) =>
+      show False from h2 ((h barber).mpr h2)
+    )
